dab3367ddbac8b58a9126b08407130753a866b8c
Maja Umek <mu2729@student.uni-lj.si>
2019-05-26 19:46:21 +0200

    Minor fixes

--- indexer/config.py
+++ indexer/config.py
@@ -1,2 +1,3 @@
 db_file = "../inverted-index.db"
 data_path = "../PA3-data/"
+results_number = 20
--- indexer/data_retrieval.py
+++ indexer/data_retrieval.py
@@ -2,14 +2,13 @@ import re
 import os
 import sqlite3
 import logging
+import time
 import indexer.config as conf
 from indexer import text_retrieval
 from indexer.text_retrieval import prepare_tokens
 
 
-def get_snippet(index, num_of_words, document_path):
-    text = text_retrieval.get_text(document_path)
-    split_text = text.split()
+def get_snippet(index, num_of_words, split_text):
     start_index = int(index - (num_of_words - 1) / 2)
     end_index = int(index + num_of_words / 2)
     # make sure indexes are not out of range
@@ -29,17 +28,20 @@ def print_results(results, query_string):
     print("Frequency | Document name                            | Snippet                                                                            ")
     print("---------- ------------------------------------------ ------------------------------------------------------------------------------------")
 
-    # print(results)
+    if not results:
+        print(" Ni rezultatov.")
     for result in results:      # result: 0 - frequencies sum, 1 - indexes, 2 - document name
         document_name = result[2]
         match = re.match("([a-zA-Z.\-]*)\.[0-9]*\.html", document_name)
         if match:
+            document_text = text_retrieval.get_text(conf.data_path + match.group(1) + "/" + document_name)
+            split_text = document_text.split()
             indexes = sorted(result[1])
             num_of_indexes = len(indexes)
             last_position = indexes[0] - 1
             snippet_string = ""
-            if indexes[0] < 4:
-                snippet_string += "..."
+            if indexes[0] > 4:
+                snippet_string += "... "
             num_of_snippets = 0
             for i in range(num_of_indexes):
                 num_of_words = 7  # number of words per snippet
@@ -50,8 +52,7 @@ def print_results(results, query_string):
                 # show max (num_of_words + 1) words in snippet (+1 if two consecutive tokens)
                 if i < num_of_indexes - 1 and indexes[i + 1] == indexes[i] + 1:
                     num_of_words += 1
-                snippet, last_position = get_snippet(indexes[i], num_of_words,
-                                                     conf.data_path + match.group(1) + "/" + document_name)
+                snippet, last_position = get_snippet(indexes[i], num_of_words, split_text)
                 snippet_string += snippet + " ... "
                 num_of_snippets += 1
             frequency_string = str(result[0])
@@ -63,12 +64,11 @@ def print_results(results, query_string):
                 "Could not get document's folder name - bad document name {}".format(document_name))
 
 
-def get_query_results(query_string):
+def get_query_results(query_string, num_of_results):
     query_tokens = [token for token, idx in text_retrieval.prepare_tokens(query_string, html=False)]
-    # print(query_tokens)
     connection = sqlite3.connect(conf.db_file)
     cursor = connection.cursor()
-    query = "SELECT * FROM Posting WHERE word IN ({})"
+    query = "SELECT word, documentName, frequency, indexes FROM Posting WHERE word IN ({})"
     cursor.execute(query.format(','.join("?" * len(query_tokens))), tuple(query_tokens))
     results = cursor.fetchall()
     connection.close()
@@ -84,11 +84,11 @@ def get_query_results(query_string):
             results_dict[doc] = [freq, idxs, doc]
 
     # sort dictionary by frequency descending
-    results = sorted(results_dict.values(), key=lambda x: x[0])
-    print_results(results, query_string)
+    results = sorted(results_dict.values(), key=lambda x: x[0], reverse=True)
+    print_results(results[:num_of_results], query_string)
 
 
-def get_query_results_sequential(query_string, data_path=conf.data_path):
+def get_query_results_sequential(query_string, num_of_results, data_path=conf.data_path):
     query_tokens = [token for token, idx in text_retrieval.prepare_tokens(query_string, html=False)]
     results_dict = {}
     for folder in os.listdir(data_path):
@@ -96,7 +96,6 @@ def get_query_results_sequential(query_string, data_path=conf.data_path):
         if os.path.isdir(full_path):
             for file in os.listdir(full_path):
                 # process file
-                # print(os.path.join(full_path, file))
                 tokens = prepare_tokens(os.path.join(full_path, file))
                 freq = 0
                 idxs = []
@@ -107,9 +106,12 @@ def get_query_results_sequential(query_string, data_path=conf.data_path):
                 if freq != 0:
                     results_dict[file] = [freq, idxs, file]
     results = sorted(results_dict.values(), key=lambda x: x[0], reverse=True)
-    print_results(results, query_string)
+    print_results(results[:num_of_results], query_string)
 
 
 if __name__ == "__main__":
-    get_query_results_sequential("sklenejo načina poročati")
-    # get_query_results("sklenejo načina poročati")
+    start_time = time.time()
+    results_num = conf.results_number
+    # get_query_results_sequential("predelovalne dejavnosti", results_num)
+    get_query_results("predelovalne dejavnosti", results_num)
+    print("Execution time: ", time.time()-start_time)
